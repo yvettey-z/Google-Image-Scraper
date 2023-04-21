447fc80207a3d1edde5c748760d64f68a0ef1a80
DodoY_529 <64227800+yvettey-z@users.noreply.github.com>
2023-04-20 22:34:46 -0400

    Update main.py
    revert the chanegs

--- main.py
+++ main.py
@@ -41,21 +41,19 @@ if __name__ == "__main__":
     image_path = os.path.normpath(os.path.join(sys.argv[1], 'photos'))
 
     #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    types = sys.argv[2]
-    for type in types:
-        search_keys = list(set([type]))
+    search_keys = list(set([sys.argv[2]]))
 
-        #Parameters
-        number_of_images = int(sys.argv[3])                # Desired number of images
-        headless = True                     # True = No Chrome GUI
-        min_resolution = (0, 0)             # Minimum desired image resolution
-        max_resolution = (9999, 9999)       # Maximum desired image resolution
-        max_missed = 10                     # Max number of failed images before exit
-        number_of_workers = 1               # Number of "workers" used
-        keep_filenames = False              # Keep original URL image filenames
+    #Parameters
+    number_of_images = int(sys.argv[3])                # Desired number of images
+    headless = True                     # True = No Chrome GUI
+    min_resolution = (0, 0)             # Minimum desired image resolution
+    max_resolution = (9999, 9999)       # Maximum desired image resolution
+    max_missed = 10                     # Max number of failed images before exit
+    number_of_workers = 1               # Number of "workers" used
+    keep_filenames = False              # Keep original URL image filenames
 
-        #Run each search_key in a separate thread
-        #Automatically waits for all threads to finish
-        #Removes duplicate strings from search_keys
-        with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_workers) as executor:
-            executor.map(worker_thread, search_keys)
+    #Run each search_key in a separate thread
+    #Automatically waits for all threads to finish
+    #Removes duplicate strings from search_keys
+    with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_workers) as executor:
+        executor.map(worker_thread, search_keys)
